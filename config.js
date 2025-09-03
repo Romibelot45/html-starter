d006c14781fde5655b230f8e770a344616da7214
Romibelot45 <Romibelot45@gmail.com>
2025-09-03 15:17:59 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Archivo de configuración
-window.APP_CONFIG = {
-  whatsapp: {
-    number: "543821450244", // número en formato internacional (sin "+")
-    message: "¡Buen4s! Me gust4rí4 cre4r un usu4rio. Mi nombre es:"
-  }
-};
