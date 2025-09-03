9d74b794d3c15cf2d169c7e96f12e8419ef90b3f
Romibelot45 <Romibelot45@gmail.com>
2025-09-03 14:49:44 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Archivo de configuración
+window.APP_CONFIG = {
+  whatsapp: {
+    number: "543821450244", // número en formato internacional (sin "+")
+    message: "¡Buen4s! Me gust4rí4 cre4r un usu4rio. Mi nombre es:"
+  }
+};
